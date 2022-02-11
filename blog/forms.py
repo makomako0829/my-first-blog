cfd185be354c2ad39ec053848bf191513c8d8a9c
makoto <makoto@example.com>
2022-02-11 17:50:28 +0900

    Added views to create/edit blog post inside the site.

---
+++ blog/forms.py
@@ -0,0 +1,12 @@
+from dataclasses import fields
+from msilib.schema import Class
+from pyexpat import model
+from django import forms
+
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    
+    class Meta:
+        model = Post
+        fields = 'title', 'text',
